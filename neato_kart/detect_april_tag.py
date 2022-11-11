bf25e546e0df5e61b0f59c4f03222ab290491c67
seungulyu <travislyu@gmail.com>
2022-11-10 22:36:53 -0500

    Add codes for pose estimate

--- neato_kart/detect_april_tag.py
+++ neato_kart/detect_april_tag.py
@@ -6,14 +6,15 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import os
-import apriltag
+import numpy.typing
+import pupil_apriltags as apriltag
 
 class RecordData(Node):
     def __init__(self, image_topic):
         super().__init__('ball_tracker')
 
         self.isImage = True
-        self.image_name = "left 0 front 72.png"
+        self.image_name = "left 10.5 front 72.png"
         self.did_draw = False
 
         self.cv_image = None                        # the latest image from the camera
@@ -47,9 +48,10 @@ class RecordData(Node):
     def run_loop(self):
         if not self.cv_image is None and not self.did_draw:
             gray = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2GRAY)
-            options = apriltag.DetectorOptions(families="tag36h11")
-            detector = apriltag.Detector(options)
-            results = detector.detect(gray)
+            detector = apriltag.Detector(families="tag36h11")
+            camera_info = {}
+            camera_info["params"] = [313.111, 310.344, 336.114, 239.242]
+            results = detector.detect(gray, estimate_tag_pose = True, camera_params=camera_info["params"], tag_size = 0.1)
 
             for r in results:
                 # extract the bounding box (x, y)-coordinates for the AprilTag
@@ -60,10 +62,10 @@ class RecordData(Node):
                 ptD = (int(ptD[0]), int(ptD[1]))
                 ptA = (int(ptA[0]), int(ptA[1]))
                 # draw the bounding box of the AprilTag detection
-                cv2.line(self.cv_image, ptA, ptB, (0, 255, 0), 1)
-                cv2.line(self.cv_image, ptB, ptC, (0, 255, 0), 1)
-                cv2.line(self.cv_image, ptC, ptD, (0, 255, 0), 1)
-                cv2.line(self.cv_image, ptD, ptA, (0, 255, 0), 1)
+                cv2.line(self.cv_image, ptA, ptB, (0, 255, 0), 2)
+                cv2.line(self.cv_image, ptB, ptC, (0, 255, 0), 2)
+                cv2.line(self.cv_image, ptC, ptD, (0, 255, 0), 2)
+                cv2.line(self.cv_image, ptD, ptA, (0, 255, 0), 2)
                 # draw the center (x, y)-coordinates of the AprilTag
                 (cX, cY) = (int(r.center[0]), int(r.center[1]))
                 cv2.circle(self.cv_image, (cX, cY), 5, (0, 0, 255), -1)
@@ -72,6 +74,8 @@ class RecordData(Node):
                 cv2.putText(self.cv_image, tagFamily, (ptA[0], ptA[1] - 15),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 print("[INFO] tag family: {}".format(tagFamily))
+
+                print(r.pose_err)
 
             cv2.imshow('video_window', self.cv_image)
             if self.isImage:
