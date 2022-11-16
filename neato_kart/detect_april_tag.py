68eac82fc90194f99f5ce4b3d22c96cf386397f9
seungulyu <travislyu@gmail.com>
2022-11-15 20:53:18 -0500

    Fix shutdown error

--- neato_kart/detect_april_tag.py
+++ neato_kart/detect_april_tag.py
@@ -7,7 +7,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import os
-import pupil_apriltags as apriltag
+#import pupil_apriltags as apriltag
+import dt_apriltags as apriltag
 import tf2_ros
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
@@ -28,6 +29,8 @@ class RecordData(Node):
         self.tf_broadcaster = TransformBroadcaster(self)
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
+        self.timestamp = None
+
         self.cv_image = None                        # the latest image from the camera
         self.image_num = 0                          # image frame number
         self.bridge = CvBridge()                    # used to convert ROS messages to OpenCV
@@ -39,20 +42,19 @@ class RecordData(Node):
             self.cv_image = cv2.imread(image_path)
 
         self.create_subscription(Image, image_topic, self.process_image, 10)
-        self.create_subscription(Image, image_topic, self.process_image, 10)
         thread = Thread(target=self.loop_wrapper)
         thread.start()
 
     def create_map_frame(self):
         
         q = quaternion_from_euler(0, 0, 0.07)
-        #test = self.tf_buffer.lookup_transform("base_link", "odom", Time())
+        test = self.tf_buffer.lookup_transform("base_link", "odom", self.timestamp)
         #print(test)
         transform = TransformStamped()
-        transform.header.stamp = self.get_clock().now().to_msg()
+        transform.header.stamp = self.timestamp
         transform.header.frame_id = 'map'
-        transform.child_frame_id = 'base_link'
-        transform.transform.translation.x = 0.63
+        transform.child_frame_id = 'odom'
+        transform.transform.translation.x = -0.63
         transform.transform.translation.y = 0.0
         transform.transform.translation.z = -0.2
         transform.transform.rotation.x = q[0]
@@ -64,7 +66,8 @@ class RecordData(Node):
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
-        #self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.timestamp = msg.header.stamp
 
     def loop_wrapper(self):
         """ This function takes care of calling the run_loop function repeatedly.
@@ -74,16 +77,17 @@ class RecordData(Node):
         #cv2.namedWindow('undistorted_window')
         while True:
             self.run_loop()
-            self.create_map_frame()
-            time.sleep(0.1)
+            #self.create_map_frame()
+            time.sleep(0.5)
 
     def run_loop(self):
         if not self.cv_image is None and not self.did_draw:
             gray = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2GRAY)
-            detector = apriltag.Detector(families="tag36h11")
+            detector = apriltag.Detector(families="tag36h11", nthreads=2)
             camera_param = [971.646825, 972.962863, 501.846472, 402.829241]
             results = detector.detect(gray, estimate_tag_pose = True, camera_params=camera_param, tag_size = 0.09)
 
+            detected_image = self.cv_image.copy()
             for r in results:
                 # extract the bounding box (x, y)-coordinates for the AprilTag
                 # and convert each of the (x, y)-coordinate pairs to integers
@@ -93,16 +97,16 @@ class RecordData(Node):
                 ptD = (int(ptD[0]), int(ptD[1]))
                 ptA = (int(ptA[0]), int(ptA[1]))
                 # draw the bounding box of the AprilTag detection
-                cv2.line(self.cv_image, ptA, ptB, (0, 255, 0), 2)
-                cv2.line(self.cv_image, ptB, ptC, (0, 255, 0), 2)
-                cv2.line(self.cv_image, ptC, ptD, (0, 255, 0), 2)
-                cv2.line(self.cv_image, ptD, ptA, (0, 255, 0), 2)
+                cv2.line(detected_image, ptA, ptB, (0, 255, 0), 2)
+                cv2.line(detected_image, ptB, ptC, (0, 255, 0), 2)
+                cv2.line(detected_image, ptC, ptD, (0, 255, 0), 2)
+                cv2.line(detected_image, ptD, ptA, (0, 255, 0), 2)
                 # draw the center (x, y)-coordinates of the AprilTag
                 (cX, cY) = (int(r.center[0]), int(r.center[1]))
-                cv2.circle(self.cv_image, (cX, cY), 5, (0, 0, 255), -1)
+                cv2.circle(detected_image, (cX, cY), 5, (0, 0, 255), -1)
                 # draw the tag family on the image
                 tagFamily = r.tag_family.decode("utf-8")
-                cv2.putText(self.cv_image, tagFamily, (ptA[0], ptA[1] - 15),
+                cv2.putText(detected_image, tagFamily, (ptA[0], ptA[1] - 15),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 print("[INFO] tag family: {}".format(tagFamily))
                 print("[INFO] tag id: {}".format(r.tag_id))
@@ -114,10 +118,10 @@ class RecordData(Node):
                 
                 print(rot.GetEulerZYX())
 
-            cv2.imshow('video_window', self.cv_image)
+            cv2.imshow('video_window', detected_image)
             #cv2.imshow('undistorted_window', img)
             if self.isImage:
-                self.did_draw = True
+                self.did_draw = False
             cv2.waitKey(5)
 
 if __name__ == '__main__':
